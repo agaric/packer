8306199d4ff7df265e8fe80de65083df50eaa2b6
Stefan Freudenberg <stefan@agaric.com>
2014-07-30 22:00:55 +0200

    Install Drupal coding standard for phpcs

--- provision-phpcs.sh
+++ provision-phpcs.sh
@@ -2,3 +2,10 @@
 if ! pear list | grep PHP_CodeSniffer &> /dev/null ; then
 	pear install PHP_CodeSniffer
 fi
+
+DRUPAL_STANDARD_DIR=/usr/share/php/PHP/CodeSniffer/Standards/Drupal
+if [ ! -d $DRUPAL_STANDARD_DIR ]; then
+	drush dl coder
+	cp -r coder/coder_sniffer/Drupal $DRUPAL_STANDARD_DIR
+	rm -r coder
+fi
