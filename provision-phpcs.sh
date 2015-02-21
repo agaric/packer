31163176585cc7c1902a05a487cf4a31d880eee3
Stefan Freudenberg <stefan@agaric.com>
2015-02-21 16:27:10 +0100

    Pin phpcs and coder versions

--- provision-phpcs.sh
+++ provision-phpcs.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 if ! pear list | grep PHP_CodeSniffer &> /dev/null ; then
-	pear install PHP_CodeSniffer
+	pear install PHP_CodeSniffer-1.5.5
 fi
 
 DRUPAL_STANDARD_DIR=/usr/share/php/PHP/CodeSniffer/Standards/Drupal
 if [ ! -d $DRUPAL_STANDARD_DIR ]; then
-	drush dl coder
+	drush dl coder-7.x-2.4
 	cp -r coder/coder_sniffer/Drupal $DRUPAL_STANDARD_DIR
 	rm -r coder
 fi
