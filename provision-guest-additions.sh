9846c1e4cb6f368e1bd24db190cc84635d2619ab
Stefan Freudenberg <stefan@agaric.com>
2015-03-28 16:25:06 +0100

    Purge packages required to build guest additions

--- provision-guest-additions.sh
+++ provision-guest-additions.sh
@@ -8,5 +8,9 @@ VBOX_ISO=VBoxGuestAdditions.iso
 mount -o loop $VBOX_ISO /mnt
 yes|sh /mnt/VBoxLinuxAdditions.run
 umount /mnt
-rm $VBOX_ISO
-
+rm -f $VBOX_ISO
+rm -rf /usr/src/vboxguest*
+apt-get -y --purge remove \
+        build-essential \
+        dkms \
+        linux-headers-amd64
